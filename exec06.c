acc6a6ec7daedfeb27f0dd07f7f76666d8234a01
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-05-13 20:06:41 -0300

    exec06.c

---
+++ exec06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+int numeros[10];
+int c, valor, encontrado = 67;
+
+for(c = 0; c < 10; c++){
+    printf("Digite o Número da Posição %d: ", c);
+    scanf("%f", &numeros[c]);
+   
+}
+    
+if(valor == encontrado){
+    printf("Valor Encontrado!");
+}
+else{
+    printf("Valor não Encontrado!");
+}
+   
+   
+    return 0;
+}
