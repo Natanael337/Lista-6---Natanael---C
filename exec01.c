96fce54657d7c20d9450b54190f086a228ad5028
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-05-13 19:11:29 -0300

    exec01.c

--- exec01.c
+++ exec01.c
@@ -9,6 +9,7 @@ for(pos = 0; pos < 10; pos++){
     printf("Digite o Número da Posição %d: ", pos);
     scanf("%d", &numeros[pos]);
 }
+printf("\nVALORES DIGITADOS: ");
 for(pos = 0; pos < 10; pos++){
     printf("\nO Número Registrado na Posição %d foi: %d", pos, numeros[pos]);
 }
